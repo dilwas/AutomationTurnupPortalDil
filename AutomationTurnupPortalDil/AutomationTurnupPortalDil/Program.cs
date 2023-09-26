a170d831b250f997df6e12a5888d1b169ad13abd
dilwas <dilhaniwas@gmail.com>
2023-09-26 23:28:16 +1300

    Initial test case for Turnup portal added

---
+++ AutomationTurnupPortalDil/AutomationTurnupPortalDil/Program.cs
@@ -0,0 +1,33 @@
+﻿﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Open chrome browser
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//Launch turnup portal and navigate to website login page
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login");
+
+//Identify username textbox and enter valide username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+//Identify password textbox and enter valide password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+//Identify the login button and click on the button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+//Check if user has logged in successfully
+IWebElement helloHari=driver.FindElement(By.XPath($"//*[@id=\"logoutForm\"]/ul/li/a"));
+
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("User has loged in successefully");
+}
+else
+{
+    Console.WriteLine("User has not loged in");
+}
