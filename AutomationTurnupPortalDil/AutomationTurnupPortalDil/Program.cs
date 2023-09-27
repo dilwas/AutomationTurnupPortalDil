c02987ce6ec41794a78457b23aa874f6e8005127
dilwas <dilhaniwas@gmail.com>
2023-09-27 17:04:07 +1300

    Added the second automation test for Time and Material module - create new

--- AutomationTurnupPortalDil/AutomationTurnupPortalDil/Program.cs
+++ AutomationTurnupPortalDil/AutomationTurnupPortalDil/Program.cs
@@ -31,3 +31,60 @@ else
 {
     Console.WriteLine("User has not loged in");
 }
+
+//Test case - Create a new Time record
+
+//Navigate to Time & Material module
+IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+administrationDropdown.Click();
+
+IWebElement tmOption = driver.FindElement(By.XPath("//a[contains(text(),'Time & Materials')]"));
+tmOption.Click();
+
+
+//Click on the create new button
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
+
+//Select time from dropdown
+IWebElement typemainCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+typemainCodeDropdown.Click();
+IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+typeCodeDropdown.Click();
+
+//Enter code 
+IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+codeTextBox.SendKeys("12340");
+
+//Enter Description
+IWebElement descriptionTextBox = driver.FindElement(By.Id("Description"));
+descriptionTextBox.SendKeys("This is a test 1");
+
+//Enter price
+IWebElement priceTextBox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+priceTextBox.SendKeys("101");
+
+//Click on the save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+
+Thread.Sleep(5000);
+//Check if a new Time record has been created successfully
+IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPageButton.Click();
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (newCode.Text == "12340")
+{
+    Console.WriteLine("New time record has been created successfully");
+}
+else
+{
+    Console.WriteLine("Time record has not created");
+}
+
+
+
+
+
+
